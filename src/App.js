da5ab8f7299a96e91f3744975d7e3e5b9ef0658f
Tomas23052 <aluno23052@ipt.pt>
2023-06-15 18:10:33 +0100

    Criação de navbar e index

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import logo from './logo.png';
-import './App.css';
+import React from 'react';
+import Index from './paginas/Index';
+import Navbar from './assets/Navbar';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
 
 
 
 function App() {
-  return (
-    <div className='navbar'>
-      <ul>
-        <li><a href="default.asp">Filmes</a></li>
-        <li><a href="news.asp">Séries</a></li>
-        <li><a href="contact.asp">Subscrições</a></li>
-        <li><a href="about.asp">Utilizadores</a></li>
-      </ul>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Esta aplicação web foi feita no intuito da Unidade Curricular Desenvolvimento Web, TMelix consiste numa aplicação que com uma subscrição, mensal ou anual nos deixa ter acesso à vasta gama de filmes e séries.</p>
-          <p>Esta aplicação foi feita por: </p>
-          <p>Francisco Soares Melo - 23155</p>
-          <p>Tomás Silva - 23052</p>
-
-        </header>
-
+  return(
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+            <Route exact path="/" element={<Index/>}/>       
+        </Routes>
       </div>
-    </div>
-
+    </BrowserRouter>
   );
 }
 

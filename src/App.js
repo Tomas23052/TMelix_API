53bbd1bf1ed428cb51ebc73ed3e0bb58de388286
Tomas23052 <aluno23052@ipt.pt>
2023-06-15 18:40:12 +0100

    Criação de rotas e pastas para todas as views

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Index from './paginas/Index';
+import Subscricoes from './paginas/Subscricoes';
+import Filmes from './paginas/Filmes';
+import Series from './paginas/Series';
+import Utilizadores from './paginas/Utilizadores';
 import Navbar from './assets/Navbar';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
@@ -13,7 +17,12 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-            <Route exact path="/" element={<Index/>}/>       
+            <Route exact path="/" element={<Index/>}/> 
+            <Route exact path="/Subscricoes" element={<Subscricoes/>}/>       
+            <Route exact path="/Filmes" element={<Filmes/>}/>       
+            <Route exact path="/Series" element={<Series/>}/>       
+            <Route exact path="/Utilizadores" element={<Utilizadores/>}/>       
+
         </Routes>
       </div>
     </BrowserRouter>
